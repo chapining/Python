7973644c52300a1e59ea43d6e1eb0067ddde1c7f
chapining <72807887+chapining@users.noreply.github.com>
2021-09-08 19:06:20 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,17 +1,20 @@
 minimum_age = 30
-minimum_balanc = 650
+minimum_balance = 350
 
 user_age = input("Сколько вам лет?")
-use_balanc = input("Сколько у вас есть деняк?")
+user_balance = input("Сколько у вас есть деняк?")
 
-int(user_age)
-int(use_balanc)
+user_age = int(user_age)
+user_balance = int(user_balance)
+
+difference_age = minimum_age - user_age
+difference_balance = minimum_balance - user_balance
 
 if user_age >= minimum_age:
 	print("Возраст у вас подходящий!")
 else: 
-	print("Вы ещё слишком молод, приходите позже.")
-if user_age >= minimum_age and use_balanc >= minimum_balanc:
+	print("Вы ещё слишком молод, приходите через", difference_age, " лет.")
+if user_age >= minimum_age and user_balance >= minimum_balance:
 	print("Всего хватает, проходите, деньги спишуться автоматически!")
-elif user_age >= minimum_age and use_balanc < minimum_balanc:
-	print("Но денег слишком мало, приходите когда будут!")
+elif user_age >= minimum_age and user_balance < minimum_balance:
+	print("Но денег слишком мало, приходите когда получите ещё", difference_balance, "!")
