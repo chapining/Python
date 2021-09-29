18baa54248793ba01a624893a93a4b965b9cbe8e
chapining <72807887+chapining@users.noreply.github.com>
2021-09-29 18:05:46 +0300

    Add files via upload

---
+++ перевод денег .py
@@ -0,0 +1,10 @@
+rub = int(input("Cколько у тебя рублей: "))
+dol = float('{:.3f}'.format(rub * 0.014))
+print (type(rub))
+print (type(dol))
+print ("А у тебя", dol , "доллоров!")
+print ("Вроде не плохо!")
+
+input()
+
+
---
+++ покупка билета.py
@@ -0,0 +1,23 @@
+minimum_age = 30
+minimum_balance = 350
+
+user_age = input("Сколько вам лет?")
+user_age = int(user_age)
+difference_age = minimum_age - user_age
+
+if user_age >= minimum_age:
+	print("Возраст у вас подходящий!")
+else: 
+	print("Вы ещё слишком молод, приходите через", difference_age, " лет.")
+if user_age > minimum_age:
+	user_balance = input("Сколько у вас есть деняк?")
+	user_balance = int(user_balance)
+	difference_balance = minimum_balance - user_balance
+	change = user_balance - minimum_balance 
+
+	if user_age >= minimum_age and user_balance >= minimum_balance:
+		print("Всего хватает, проходите, деньги спишуться автоматически!")
+		print("У вас останется", change, "!")
+	elif user_age >= minimum_age and user_balance < minimum_balance:
+		print("Но денег слишком мало, приходите когда получите ещё", difference_balance, "!")
+		
